2f7e09a87a20d8fb888b9b1acac94a8cd463839a
tsung_macbook <tctsung@nyu.edu>
2023-03-19 16:48:04 -0400

    save pickle file for each cleaning steps

--- data_cleaner.py
+++ data_cleaner.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import re
 from sklearn.preprocessing import StandardScaler
+import pickle
 class Cleaner:
 	"""
 	For data cleaning
@@ -9,7 +10,7 @@ class Cleaner:
 	def __init__(self, df):
 		assert isinstance(df, pd.DataFrame), "Input data must be a pandas DataFrame."
 		self.df = df
-	# def __rp
+	# def __repr__
 	def rm_na_cols(self, na_percentage):
 		assert 0 <= na_percentage <= 1, "na_percentage must be between 0 and 1"
 		cnt = int(self.df.shape[0] * na_percentage)
@@ -21,6 +22,12 @@ class Cleaner:
 		"""
 		if nm:
 			self.df.dropna(subset=[nm], inplace=True)
+	def binary_to_int(self):
+		"""
+		TODO: turn binary variable to integer
+		"""
+		int_col = self.df.columns[self.df.apply(lambda x:len(x.dropna().unique()))==2].to_list() # find unique val=2 not including NaN
+		self.df.loc[:,int_col] = self.df.loc[:,int_col].astype('Int64')
 	def fix_colname(self):
 		"""
 		TODO: In column names, turn uncommon symbols to underline. Spaces will be replaced by underline
@@ -32,6 +39,7 @@ class Cleaner:
 		self.drop_duplicates(inplace=True) # rm duplicate rows
 		self.rm_no_outcome(nm)             # drop rows with no outcome
 		self.rm_na_cols(na_percentage)     # drop cols with missingness > na_percentage
+		self.binary_to_int()               # turn binary variables to integer
 		self.fix_colname()                 # rm weird symbols from column names
 def overview(df):
 	"""
@@ -54,11 +62,26 @@ def standardization(df):
 
 
 
-# def auto_dtype(df):
-# 	"""
-	
-# 	"""
-# 	return df.apply(lambda x:x.dtype)
+
+def save_py(obj, location):
+	"""
+	TODO: to save a python object
+	:param obj: object name
+	:param location: location to save the file
+	"""
+	if location[-4:] != '.pkl': location += '.pkl'  # add file extension
+	savefile = open(f'{location}', 'wb')
+	pickle.dump(obj, savefile)
+	savefile.close()
+def load_py(location):
+	file = open(f'{location}', 'rb')     # open a file, where you stored the pickled data
+	data = pickle.load(file)
+	file.close()
+	return data
+
+
+
+
 
 
 
